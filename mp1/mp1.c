7932c869237eb768c6eb4df92a566d7f91009104
Drew Cross <mstngdrew212@gmail.com>
2012-09-16 00:01:29 -0500

    testing procfs stuffs

--- mp1/mp1.c
+++ mp1/mp1.c
@@ -2,27 +2,60 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/proc_fs.h>
 
-//THIS FUNCTION GETS EXECUTED WHEN THE MODULE GETS LOADED
-//NOTE THE __INIT ANNOTATION AND THE FUNCTION PROTOTYPE
-int __init my_module_init(void)
+
+#define PROCFS_NAME "mp1"
+
+struct proc_dir_entry* proc_file_g;
+
+int
+procfile_read(
+   char * buffer,
+   char ** buffer_location,
+   off_t offset,
+   int buffer_length,
+   int * eof,
+   void * data
+   )
 {
-   //THE EQUIVALENT TO PRINTF IN KERNEL SPACE
-   printk(KERN_ALERT "MODULE LOADED\n");
-   return 0;   
+    //TODO
 }
 
-//THIS FUNCTION GETS EXECUTED WHEN THE MODULE GETS UNLOADED
-//NOTE THE __EXIT ANNOTATION AND THE FUNCTION PROTOTYPE
-void __exit my_module_exit(void)
+int __init
+my_module_init(void)
 {
-   printk(KERN_ALERT "MODULE UNLOADED\n");
+    printk(KERN_ALERT "MODULE LOADED\n");
+    proc_file_g = create_proc_entry(procfs_name, 0666, NULL);
+
+    if(proc_file_g == NULL)
+    {
+        remove_proc_entry(PROCFS_NAME, &proc_root);
+        printk(KERN_ALERT "Error: Could not initialize /proc/%s\n", PROCFS_NAME);
+        return -ENOMEM;
+    }
+
+    //TODO - figure out why this needs to happen?
+    proc_file_g->read_proc = procfile_read;
+    proc_file_g->owner = THIS_MODULE;
+    proc_file_g->mode = S_IFREG | S_IRUGO;
+    proc_file_g->uid = 0;
+    proc_file_g->gid = 0;
+    proc_file_g->size = 37;
+
+    printk(KERN_INFO "/proc/%s created\n", PROCFS_NAME);
+
+    return 0;
 }
 
-//WE REGISTER OUR INIT AND EXIT FUNCTIONS HERE SO INSMOD CAN RUN THEM
-//MODULE_INIT AND MODULE_EXIT ARE MACROS DEFINED IN MODULE.H
+void __exit
+my_module_exit(void)
+{
+    remove_proc_entry(PROCFS_NAME, &proc_root);
+    printk(KERN_ALERT "MODULE UNLOADED\n");
+}
+
 module_init(my_module_init);
 module_exit(my_module_exit);
 
-//THIS IS REQUIRED BY THE KERNEL
 MODULE_LICENSE("GPL");
